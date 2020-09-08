7146d03cde012f09096e197571e500017265c91e
Reppi21 <70944014+Reppi21@users.noreply.github.com>
2020-09-08 15:58:52 +0700

    Add files via upload

---
+++ ReppiAlpiani_XII RPL C_UTSsept2020.java
@@ -0,0 +1,22 @@
+
+package ulangan;
+
+
+public class ulangan1 {
+    public static void main(String[] args) {
+        String str = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z ";
+        
+        char ch1 = str.charAt(34);
+        char ch2 = str.charAt(8);
+        char ch3 = str.charAt(30);
+        char ch4 = str.charAt(30);
+        char ch5 = str.charAt(16);
+        char ch6 = str.charAt(51);
+        char ch7 = str.charAt(0);
+        char ch8 = str.charAt(22);
+        char ch9 = str.charAt(30);
+        char ch10 = str.charAt(26);
+        
+        System.out.println(" " +ch1+ch2+ch3+ch4+ch5+ch6+ch7+ch8+ch9+ch10);
+    }
+}
